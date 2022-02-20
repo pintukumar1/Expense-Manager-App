7344d6716f94590d11a8e1d17f87235e50c8180d
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-02-20 08:28:51 +0530

    dropdown added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
     const expenses = [
@@ -28,9 +30,14 @@ const App = () => {
         },
       ];
 
+      const addExpenseHandler = expense => {
+        console.log('In app.js')
+        console.log(expense)
+      }
+
   return (
     <div>
-      <h2>Let's get Started</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}/>
     </div>
   );
