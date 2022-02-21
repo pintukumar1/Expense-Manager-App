815978d8a8c28d0b4b82db41fb004032c4339e76
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-02-21 09:47:55 +0530

    chart added project complete

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesChart from './ExpensesChart'
 import './Expenses.css'
 import ExpenseList from './ExpensesList'
 
@@ -20,6 +21,7 @@ const Expenses = (props) => {
                 selected={filteredYear} 
                 onChangeFilter={filterChangeHandler} 
             />
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpenseList items={filteredExpenses} />
         </Card>
     )
